da48b07c761c577edf832d1ddf7dbb7af9238d6d
Reza Omidian <romidian70@gmail.com>
2020-04-05 19:12:35 +0430

    skype call about tokens

--- services/authorization.js
+++ services/authorization.js
@@ -1,10 +1,21 @@
+const jwt = null;
 const refreshToken = {
     sign: (payload) => {
         const token = 'jsonwebtoken generated token';
         return token;
     },
-    verify() {
+    verify(req, res, next) {
 
+        checkHeaders();
+
+        jwt.verify(token[1], `secretKey☼"ç♂♦♣2B82☺54å☺2©Ë™♦4321QWEd32$@#$#@6^^!`, function (err, decoded) {
+            if (err)
+                res.status(401).send({ERR: `Token is not valid`});
+            if (decoded) {
+                req.token = decoded;
+                next();
+            }
+        });
     }
 };
 const accessToken = {
@@ -13,7 +24,7 @@ const accessToken = {
         return token;
     },
     verify() {
-
+        checkHeaders();
     }
 };
 const tempToken = {
@@ -22,8 +33,25 @@ const tempToken = {
         return token;
     },
     verify() {
+        checkHeaders();
 
     }
+};
+
+
+const checkHeaders = () => {
+    if (!req.headers[`authorization`])
+        return res.status(401).send({ERR: `Authorization is required`});
+
+    const header = req.headers[`authorization`];
+    if (!header.startsWith('Bearer')) {
+        return res.status(400).send({ERR: `Token is not in correct format`});
+    }
+    const token = header.split(' ');
+    if (!token[1]) {
+        return res.status(400).send({ERR: `Token is not in correct format`});
+    }
+
 };
 
 
