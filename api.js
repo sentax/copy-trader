75530e06cd22c4d331be0dcfc3d6cdfc05837034
M. Bagher Abiat <zorofight94@gmail.com>
2020-04-05 14:51:10 +0430

    packages added

--- api.js
+++ api.js
@@ -1,25 +1,22 @@
 const fastify = require('fastify')({
-        logger: true
-    }),
-    authService = require('./services/authorization'),
-    Models = require('./models/__modelLoader');
-
+		logger : { prettyPrint: true }
+	}),
+	authService = require('./services/authorization'),
+	Models = require('./models/__modelLoader');
 
 //decorate fastify global variables
 fastify.decorate('DB', Models);
 fastify.decorate('AuthService', authService);
 
-
 //register routes
 fastify.register(require(`./routes/auth/authorize`));
 
-
 //start api server
 (async () => {
-    try {
-        await fastify.listen(1330)
-    } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
+	try {
+		await fastify.listen(1330);
+	} catch (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
 })();
--- models/Users.js
+++ models/Users.js
@@ -1,20 +1,56 @@
-const mongoose = require('mongoose'), uniqueValidator = require('mongoose-unique-validator'),
-    objId = mongoose.Types.ObjectId, Mixed = mongoose.Schema.Types.Mixed;
-const schema = new mongoose.Schema(
-    {
-        _id: {type: String},
-        cA: {type: Date, default: Date.now},
-        uA: {type: Date, default: Date.now},
-        email: {type: String, required: true, unique: true},
+const mongoose = require('mongoose'),
+	uniqueValidator = require('mongoose-unique-validator'),
+	objId = mongoose.Types.ObjectId,
+	Mixed = mongoose.Schema.Types.Mixed;
+const schema = new mongoose.Schema({
+	cA  : { type: Date, default: Date.now },
+	uA  : { type: Date, default: Date.now },
+	em  : { type: String, required: true, unique: true }, // email
+	un  : {
+		type     : String,
+		required : true,
+		unique   : true,
+		trim     : true
+	},
+	bn  : {
+		// base name
+		type : String,
+		trim : true
+	},
+	pwd : {
+		//password
+		type     : String,
+		required : true,
+		trim     : true,
+		validate(value) {
+			if (value.length < 6) throw new Error('The Password length should be more than 6 chars');
+		}
+	},
+	pn  : {
+		//phone number
+		type : Number
+	},
+	pm  : {
+		// permission
+		type : String
+	},
+	tfa : {
+		//boolean
+		type    : Boolean,
+		default : false
+	},
 
-        meta: {type: Mixed}
-    }
-);
+	tfs : {
+		// tfa secret key
+		type   : String,
+		unique : true
+	},
+	mt  : { type: Mixed }
+});
 //validate unique records
 schema.plugin(uniqueValidator);
 //set _id field before creating each record in database
-schema.pre('save', function (next) {
-    this._id = new objId();
-    next()
-})
+schema.pre('save', function(next) {
+	next();
+});
 module.exports = schema;
