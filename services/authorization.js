f18c2f50098df40d04dc8b5dde75050458e19483
Reza Omidian <romidian70@gmail.com>
2020-04-21 14:29:44 +0430

    authorization.js changes access tokens are stored in config.json redis integration is not correct

--- services/authorization.js
+++ services/authorization.js
@@ -4,119 +4,118 @@
  */
 
 // Dependencies
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken'),
+    config = require('../config');
 
-// Private key define
-const privateKey = '4tet#Ebb&CW&h4bwK+9a#fm!';
 
 // Access token generate and verify function
 const accessToken = {
-	sign   : (payload) => {
-		return jwt.sign(
-			{
-				...payload
-			},
-			privateKey,
-			{ expiresIn: 4 * (60 * 60) }
-		); // Expires in 4h
-	},
-	verify(req, res) {
-		// Check token Correctivity
-		const token = checkHeaders(req, res);
-		jwt.verify(token, privateKey, function(err, decoded) {
-			if (err) return res.status(401).send({ ERR: `Token is not valid` });
-			if (decoded) {
-				req.token = decoded;
-				// next();
-			}
-		});
-	}
+    sign: (payload) => {
+        return jwt.sign(
+            {
+                ...payload
+            },
+            config.keys.access,
+            {expiresIn: 4 * (60 * 60)}
+        ); // Expires in 4h
+    },
+    verify(req, res) {
+        // Check token Correctivity
+        const token = checkHeaders(req, res);
+        jwt.verify(token, config.keys.access, function (err, decoded) {
+            if (err) return res.status(401).send({ERR: `Token is not valid`});
+            if (decoded) {
+                req.token = decoded;
+                next();
+            }
+        });
+    }
 };
 
 // Temp token generate and verify function
 const tempToken = {
-	sign   : (payload) => {
-		return jwt.sign(
-			{
-				...payload
-			},
-			privateKey,
-			{ expiresIn: 1 * (60 * 60) }
-		); // Expires in 1h
-	},
-	verify(req, res, next) {
-		// Check token Correctivity
-		const token = checkHeaders(req, res);
-		jwt.verify(token, privateKey, function(err, decoded) {
-			if (err) return res.status(401).send({ ERR: `Token is not valid` });
-			if (decoded) {
-				req.token = decoded;
-				// next();
-			}
-		});
-	}
+    sign: (payload) => {
+        return jwt.sign(
+            {
+                ...payload
+            },
+            config.keys.temp,
+            {expiresIn: 1 * (60 * 60)}
+        ); // Expires in 1h
+    },
+    verify(req, res, next) {
+        // Check token Correctivity
+        const token = checkHeaders(req, res);
+        jwt.verify(token, config.keys.temp, function (err, decoded) {
+            if (err) return res.status(401).send({ERR: `Token is not valid`});
+            if (decoded) {
+                req.token = decoded;
+                // next();
+            }
+        });
+    }
 };
 
 // Refresh token generate and verify function
 const refreshToken = {
-	sign   : async (payload, fastify) => {
-		let key = `REF:${payload._id}`;
-		// const tries = await syncRedisGet(fastify, key);
-		const token = jwt.sign(
-			{
-				...payload
-			},
-			privateKey,
-			{ expiresIn: 24 * (60 * 60) * 30 } // Expires in 1M
-		);
-		await fastify.Redis.set(key, token, 'EX', 24 * (60 * 60) * 30); //Expires in 1M
+    sign: async (payload, fastify) => {
+        let key = `REF:${payload._id}`;
+        // const tries = await syncRedisGet(fastify, key);
+        const token = jwt.sign(
+            {
+                ...payload
+            },
+            config.keys.refresh,
+            {expiresIn: 24 * (60 * 60) * 30} // Expires in 1M
+        );
+        await fastify.Redis.set(key, token, 'EX', 24 * (60 * 60) * 30); //Expires in 1M
 
-		return await token;
-	},
+        return await token;
+    },
 
-	verify(req, res, next) {
-		// Check token Correctivity
-		const token = checkHeaders(req, res);
-		jwt.verify(token, privateKey, function(err, decoded) {
-			if (err) return res.status(401).send({ ERR: `Token is not valid` });
-			if (decoded) {
-				req.token = decoded;
-				next();
-			}
-		});
-	}
+    verify(req, res, next) {
+        // Check token Correctivity
+        const token = checkHeaders(req, res);
+        jwt.verify(token, config.keys.refresh, function (err, decoded) {
+            if (err) return res.status(401).send({ERR: `Token is not valid`});
+            if (decoded) {
+                req.token = decoded;
+                next();
+            }
+        });
+    }
 };
 
 // Check correct header function
 const checkHeaders = (req, res) => {
-	// Check authorization header found
-	if (!req.headers['authorization']) return res.status(401).send({ ERR: `Authorization is required` });
+    // Check authorization header found
+    if (!req.headers['authorization']) return res.status(401).send({ERR: `Authorization is required`});
 
-	// Check if header start with Bearer
-	const header = req.headers['authorization'];
-	if (!header.startsWith('Bearer')) return res.status(400).send({ ERR: `Token is not in correct format` });
+    // Check if header start with Bearer
+    const header = req.headers['authorization'];
+    if (!header.startsWith('Bearer')) return res.status(400).send({ERR: `Token is not in correct format`});
 
-	// Check if token is !null
-	const token = header.split(' ');
-	if (!token[1]) return res.status(400).send({ ERR: `Token is not in correct format` });
-	else return token[1];
+    // Check if token is !null
+    const token = header.split(' ');
+    if (!token[1]) return res.status(400).send({ERR: `Token is not in correct format`});
+    else return token[1];
 };
 
 // Check Token Exists
 const syncRedisGet = async (fastify, key) => {
-	return new Promise((resolve, reject) => {
-		fastify.Redis.get(key, async (err, val) => {
-			if (err) {
-				reject(err);
-			}
-			resolve(val);
-		});
-	});
+    return new Promise((resolve, reject) => {
+        fastify.Redis.get(key, async (err, val) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(val);
+        });
+    });
 };
 
 // Export modules
 module.exports = {
-	accessToken,
-	tempToken,
-	refreshToken
+    accessToken,
+    tempToken,
+    refreshToken
 };
