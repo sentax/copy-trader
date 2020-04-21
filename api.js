8f9c77e3b684ea8a6808b6d9d98f8bcb1a7876b4
Reza Omidian <romidian70@gmail.com>
2020-04-21 14:17:36 +0430

    User model changes, config.json added to project

--- api.js
+++ api.js
@@ -1,11 +1,11 @@
 const fastify = require('fastify')({
-		logger : true
-	}),
-	authService = require('./services/authorization'),
-	fs = require('fs'),
-	path = require('path'),
-	Models = require('./models/__modelLoader'),
-	redis = require('redis');
+        logger: true
+    }),
+    authService = require('./services/authorization'),
+    fs = require('fs'),
+    path = require('path'),
+    Models = require('./models/__modelLoader'),
+    redis = require('redis');
 
 //decorate fastify global variables
 fastify.decorate('DB', Models);
@@ -18,36 +18,37 @@ fastify.decorate('AuthService', authService);
 // fastify.register(require(`./routes/auth/register`));
 
 getFilesFromDir(path.resolve(__dirname, 'routes'), [
-	'.js'
+    '.js'
 ]).map((file) => {
-	fastify.register(require(`./routes/${file}`));
+    fastify.register(require(`./routes/${file}`));
 });
 
-//just vopied from stackoverflow dont trust this fucking code
+//just copied from stackoverflow.com dont trust this fucking code
 function getFilesFromDir(dir, fileTypes) {
-	var filesToReturn = [];
+    var filesToReturn = [];
 
-	function walkDir(currentPath) {
-		var files = fs.readdirSync(currentPath);
-		for (var i in files) {
-			var curFile = path.join(currentPath, files[i]);
-			if (fs.statSync(curFile).isFile() && fileTypes.indexOf(path.extname(curFile)) != -1) {
-				filesToReturn.push(curFile.replace(dir, ''));
-			} else if (fs.statSync(curFile).isDirectory()) {
-				walkDir(curFile);
-			}
-		}
-	}
-	walkDir(dir);
-	return filesToReturn;
+    function walkDir(currentPath) {
+        var files = fs.readdirSync(currentPath);
+        for (var i in files) {
+            var curFile = path.join(currentPath, files[i]);
+            if (fs.statSync(curFile).isFile() && fileTypes.indexOf(path.extname(curFile)) != -1) {
+                filesToReturn.push(curFile.replace(dir, ''));
+            } else if (fs.statSync(curFile).isDirectory()) {
+                walkDir(curFile);
+            }
+        }
+    }
+
+    walkDir(dir);
+    return filesToReturn;
 }
 
 //start api server
 (async () => {
-	try {
-		await fastify.listen(1330);
-	} catch (err) {
-		fastify.log.error(err);
-		process.exit(1);
-	}
+    try {
+        await fastify.listen(1330);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
 })();
--- models/Users.js
+++ models/Users.js
@@ -1,67 +1,95 @@
 const mongoose = require('mongoose'),
-	bcrypt = require('bcryptjs'),
-	{ authenticator } = require('otplib'),
-	uniqueValidator = require('mongoose-unique-validator'),
-	Mixed = mongoose.Schema.Types.Mixed;
-const schema = new mongoose.Schema({
-	cA  : { type: Date, default: Date.now },
-	uA  : { type: Date, default: Date.now },
-	em  : { type: String, required: true, unique: true, lowercase: true }, // email
-	un  : {
-		type      : String,
-		required  : true,
-		unique    : true,
-		trim      : true,
-		lowercase : true
-	},
-	bn  : {
-		// base name
-		type    : String,
-		trim    : true,
-		default : null
-	},
-	pwd : {
-		//password
-		type     : String,
-		required : true,
-		trim     : true,
-		validate(value) {
-			if (value.length < 6) throw new Error('The Password length should be more than 6 chars');
-		}
-	},
-	pn  : {
-		//phone number
-		type    : Number,
-		default : null
-	},
-	pm  : {
-		// permission
-		type    : String,
-		default : 'U'
-	},
-	tfa : {
-		//boolean
-		type    : Boolean,
-		default : false
-	},
+    bcrypt = require('bcryptjs'),
+    config = require('../config'),
+    uniqueValidator = require('mongoose-unique-validator'),
+    Mixed = mongoose.Schema.Types.Mixed,
+    schema = new mongoose.Schema({
+        cA: {
+            //created At
+            type: Date,
+            default: Date.now
+        },
+        uA: {
+            //updated At
+            type: Date,
+            default: Date.now
+        },
+        em: {
+            // email
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+        un: {
+            //username
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true
+        },
+        bn: {
+            // base name
+            type: String,
+            trim: true,
+            default: null
+        },
+        pwd: {
+            //password
+            type: String,
+            required: true,
+            trim: true,
+            validate(value) {
+                if (value.length < 6) throw new Error('The Password length should be more than 6 chars');
+            }
+        },
+        pn: {
+            //phone number
+            type: Number,
+            default: null
+        },
+        pm: {
+            // permission
+            type: String,
+            default: 'U',
+            required: true,
+            validate(value) {
+                //validate permission from defined list. this will reduce conflicts and security issues
+                if (config["user-types"].indexOf(value) < 0) throw new Error('Undefined user type');
+            }
+        },
+        tfa: {
+            //boolean
+            type: Boolean,
+            default: false,
+            required: true
+        },
 
-	tfs : {
-		// tfa secret key
-		type    : String,
-		default : authenticator.generateSecret()
-	},
-	st  : { type: Boolean, default: true },
-	mt  : { type: Mixed, default: null }
-});
+        tfs: {
+            // tfa secret key
+            type: String,
+            required: true
+        },
+        st: {
+            //status
+            type: Boolean,
+            default: true
+        },
+        mt: {
+            //meta data
+            type: Mixed,
+            default: null
+        }
+    });
 //validate unique records
 schema.plugin(uniqueValidator);
-schema.pre('save', async function(next) {
-	if (this.isModified('pwd')) {
-		// hashing the password
-
-		this.pwd = await bcrypt.hash(this.pwd, 8);
-	}
-	next();
+schema.pre('save', async function (next) {
+    if (this.isModified('pwd')) {
+        // hashing the password
+        this.pwd = await bcrypt.hash(this.pwd, 8);
+    }
+    next();
 });
 
 module.exports = schema;
